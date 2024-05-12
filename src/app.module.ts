600ffc5a4a9aa07a22ca31db083b6b91de03373a
Thorn Chorn <chornthorn.kh@gmail.com>
2024-05-12 14:08:13 +0700

    feat: Complete keycloak configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { CategoryModule } from './category/category.module';
+import { ConfigModule } from '@nestjs/config';
+import { ProductModule } from './product/product.module';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_NAME: Joi.string().required(),
+        DB_USER: Joi.string().required(),
+        DB_PASS: Joi.string().required(),
+      }),
+    }),
+    CategoryModule,
+    ProductModule,
+  ],
 })
 export class AppModule {}
